255fb4d8c192645bae79eec01771895d8485f2d1
khoi2408 <lmkhoi2k@gmail.com>
2024-05-15 00:33:34 +0700

    Creat configurator page, Drag & Drop image and customize color

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,20 +1,48 @@
-import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
-import { z } from "zod";
+import { createUploadthing, type FileRouter } from 'uploadthing/next'
+import { z } from 'zod'
+import sharp from 'sharp'
+import { db } from '@/db'
 
-const f = createUploadthing();
+const f = createUploadthing()
 
-const auth = (req: Request) => ({ id: "fakeId" });
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: "4MB" } })
+  imageUploader: f({ image: { maxFileSize: '4MB' } })
     .input(z.object({ configId: z.string().optional() }))
     .middleware(async ({ input }) => {
-      return { input };
+      return { input }
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      const { configId } = metadata.input;
-      return { configId };
-    }),
-} satisfies FileRouter;
+      const { configId } = metadata.input
 
-export type OurFileRouter = typeof ourFileRouter;
+      const res = await fetch(file.url)
+      const buffer = await res.arrayBuffer()
+
+      const imgMetadata = await sharp(buffer).metadata()
+      const { width, height } = imgMetadata
+
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        })
+
+        return { configId: configuration.id }
+      } else {
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        })
+
+        return { configId: updatedConfiguration.id }
+      }
+    }),
+} satisfies FileRouter
+
+export type OurFileRouter = typeof ourFileRouter
